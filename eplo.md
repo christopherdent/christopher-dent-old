3c78df951e79d2575c090925c410262d394ac7dc
Christopher Dent <christopherdent01@gmail.com>
2021-02-04 20:47:48 -0500

    Added GH social

---
+++ eplo.md
@@ -0,0 +1,10 @@
+# Christopher Dent's Portfolio Web App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Chris Dent's latest web app is the place to be if you want to see his recent projects, blogs, highlights and life story.  
+
+
+npm install to start
+npm start to run locally
+npm run deploy to redeploy to GH Pages.
--- src/components/Social.js
+++ src/components/Social.js
@@ -14,6 +14,7 @@ const Social = (props) => {
       <SocialIcon url="https://www.linkedin.com/in/christopher-james-dent/" id='blog' style={{ height: 100, width: 100 }} />
       <SocialIcon url="https://www.facebook.com/christopher.j.dent" id='blog' style={{ height: 100, width: 100 }} />
       <SocialIcon url="https://www.youtube.com/channel/UCZBxvf3wO37Ymx-mO8tPwLQ" id='blog' style={{ height: 100, width: 100 }} />
+      <SocialIcon url="https://github.com/christopherdent" id='blog' style={{ height: 100, width: 100 }} />
       <SocialIcon url="mailto: christopherdent01@gmail.com" id="blog" style={{ height: 100, width: 100 }} />
 
       <br />
