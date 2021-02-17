e0b0c5c281f2dbca2040a681d7a711f1773df1a0
Christopher Dent <christopherdent01@gmail.com>
2021-02-16 22:21:22 -0500

    Minor tweaks to about info

--- src/about_data.js
+++ src/about_data.js
@@ -2,15 +2,15 @@ const medata = [
 
 {
     title: 'My Experience',
-    description: "I graduated from college having majored in political science and history (double major) at McGill University in Montreal, Canada. I've been working in project management pretty much ever since (approx. 12 years experience) while learning code in my free time."
+    description: "I graduated from college having majored in political science and history (double major) at McGill University in Montreal, Canada. I've been working in various project management roles pretty much ever since (approx. 12 years experience) while learning code - my real passion - in my free time."
   },
  {
    title: 'My Skillset',
-   description: "I possess a sharp eye for detail, which I use to find even the smallest errors in my code (usually!) I work well under pressure and can produce high-quality work in short periods of time.  I have strong interpersonal skills and work with a wide variety of people in a wide variety of regions (Canada, Scotland, Washington DC, etc.)"
+   description: "I possess a sharp eye for detail, which I use to find even the smallest errors in my code (usually!) I work well under pressure and can produce high-quality work in short periods of time.  I have strong interpersonal skills and work with a wide variety of people in a many different regions (Canada, Scotland, Washington DC, etc.)"
   },
   {
     title: 'My Goals',
-    description: "I am looking for any and all opportunities to put my coding skills to good use. I'm fascinated with the concept of creating something from nothing and hope that it can become more than just a hobby someday."
+    description: "I am looking for any and all opportunities to put my coding skills to good use. I'm fascinated with the concept of creating programs from nothing but code. Hoping to make it more than just a hobby someday soon."
   },
 
 
--- src/components/LearnMore.js
+++ src/components/LearnMore.js
@@ -14,13 +14,7 @@ const LearnMore = (props) => {
 <td></td>
 <td>
      <p className="bio-text"> My name's Chris and I currently live in St. Augustine, Florida.  I live here with my
-       wife, Angelica Garcia-Dent, our dog Chilito and my cat Calvin (she denies ownership of "that cat").  I grew up in a house in the woods in Sag Harbor, NY (before it was rich) and attended
-       Pierson High School there.  When I finished that I was excited to move away for college and got accepted to McGill University where I studied Political Science with
-       a double major in History.  After briefly returning to Sag, I got a job at a great company in the DC area and moved there to do it; however, by then I had my sights
-       set on moving to the UK (my dad is from there) so I just planned to save up in DC until I could afford to make the big move which I did in 2010.  I worked a contract
-       job as a project analyst at the Royal Bank of Scotland for about a year and then moved on to be an IT Project Officer at the City of Edinburgh Council.  I loved both
-       roles and nearly stayed at the Council for good, but in 2013 I got an offer from my old company in DC that I really couldn't say no to and back I went to DC to a company
-       I'm still with today (albeit, working for them remotely here in St. Augustine).
+       wife, Angelica and our pomeranian Chilito.  I grew up in a house in the woods in Sag Harbor, NY (before it was rich) and attended Pierson High School there.  When I finished that I was excited to move away for college and got accepted to McGill University where I studied Political Science with a double major in History.  After briefly returning to Sag Harbor, I got a job at a great company in the DC area and moved there to do it.  After a few years in the DC area (2006 - 2010) I moved to the UK, where my Dad is from.  Over there I worked a contract job as a project analyst at the Royal Bank of Scotland for about a year and then moved on to be an IT Project Officer at the City of Edinburgh Council.  I loved both roles and nearly stayed at the Council for good, but in 2013 I got an offer from my old company in DC that I really couldn't say no to and back I went to DC to a company I'm still with today (working for them remotely here in St. Augustine).
     </p>
 </td>
 </tr>
@@ -30,12 +24,10 @@ const LearnMore = (props) => {
       I say back because when I was a super-nerd teen, an aunt gave me an old Gateway PC - a 386 processor with I think 4MB of RAM (I saved all summer to upgrade it to 8) and
       maybe a 200MB hard dive, if that, and I totally lost myself in it.  First by making liberal use of AOL free trial disks, then joining chat-rooms, to eventually chatting
       about how to build computers (which I did) and finally, how to build software.  The height of this was a text based game (I SO wish I could recover) I wrote on
-      C++ based on my middle school pals and distributed to them all on 3.5" floppies.  Good times.  Then, I went to college, majored in something completely unrelated and forgot
-      all about it - something I have been regretting for years.  The choice was clear, I wanted to learn to code.
+      C++ based on my middle school pals and distributed to them all on 3.5" floppies.  Good times.  Then, I went to college, majored in something completely unrelated and forgot all about it - something I have been regretting for years.  The choice was clear, I wanted to learn to code.
     </p>
     <p className="bio-text">
-     I spent the last year or so in Flatiron School's Self-Paced Online Coding Bootcamp and graduated in December, 2020 as a full-fledged software engineer.  So welcome to my portfolio site which contains the fruits of my studies in the "My Projects" section below.
-     I do my coding on Linux and I can work with Ruby/Rails, Rack (Sinatra), JavaScript, React/Redux and some basic C++, but I'm learning more every day, and that is my favorite thing about coding.
+     I spent the last year or so in Flatiron School's Self-Paced Online Coding Bootcamp and graduated in December, 2020 as a full-fledged software engineer.  So welcome to my portfolio site which contains the fruits of my studies in the "My Projects" section above.  I do my coding on Linux and I can work with Ruby/Rails, Rack (Sinatra), JavaScript, React/Redux and some basic C++, but I'm learning more every day, and that is my favorite thing about coding.
     </p>
     </div>
   )
