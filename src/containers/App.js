c40107273851a834b7cb794d3932a80af0f05607
christopherdent <christopherdent01@gmail.com>
2021-04-24 15:56:21 -0400

    center title better

--- src/containers/App.js
+++ src/containers/App.js
@@ -54,9 +54,9 @@ render() {
       <br />
       <center>
     <Image src='https://defaultcustomheadersdata.files.wordpress.com/2016/07/city1.jpg?resize=2000,300' fluid />
-    <br /><br />
+    <br /><br /><br />
 <a href="mailto: christopherdent01@gmail.com">  <h1 className='page-title'> Christopher Dent </h1></a></center>
-      <br /><br />
+      <br />
 <ColoredLine color="steelblue" />
       <ProjectsList
       listProjects={this.listProjects()}
