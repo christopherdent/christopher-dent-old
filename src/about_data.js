74fec377a244acfae6ad8ce6649c9c040ef4956a
christopherdent <christopherdent01@gmail.com>
2021-06-02 20:10:25 -0400

    minor aboutme updates

--- src/about_data.js
+++ src/about_data.js
@@ -2,11 +2,11 @@ const medata = [
 
 {
     title: 'My Experience',
-    description: "I graduated from college having majored in political science and history (double major) at McGill University in Montreal, Canada. I've been working in various project management roles pretty much ever since (approx. 12 years experience) while learning code - my real passion - in my free time."
+    description: "I graduated from college having majored in political science and history (double major) at McGill University in Montreal, Canada. I've been working in various project management roles pretty much ever since (approx. 12 years experience) while learning code in my spare time."
   },
  {
    title: 'My Skillset',
-   description: "I possess a sharp eye for detail, which I use to find even the smallest errors in my code (usually!) I work well under pressure and can produce high-quality work in short periods of time.  I have strong interpersonal skills and work with a wide variety of people in a many different regions (Canada, Scotland, Washington DC, etc.)"
+   description: "I possess a sharp eye for detail, which I use to find even the smallest errors in my code.  I work well under pressure and can produce high-quality work in short periods of time.  I have strong interpersonal skills and have worked with a wide variety of people in a many different regions (Canada, Scotland, Washington DC, etc.)"
   },
   {
     title: 'My Goals',
