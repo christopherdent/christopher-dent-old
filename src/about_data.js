67a4e48083ff4f3bcaccbb752ad9061d368b9cdf
christopherdent <christopherdent01@gmail.com>
2021-06-17 22:06:11 -0400

    minor fix to about me tiles

--- src/about_data.js
+++ src/about_data.js
@@ -1,18 +1,30 @@
+
+// const resume = "resume"
+// const reslink = resume.link("https://drive.google.com/file/d/113sUS6Hpbne3LYTqBJzVy2Guo2fQrj20/view?usp=sharing")
+
+
+
 const medata = [
 
 {
     title: 'My Experience',
-    description: "I graduated from college having majored in political science and history (double major) at McGill University in Montreal, Canada. I've been working in various project management roles pretty much ever since (approx. 12 years experience) while learning code in my spare time."
+    description: "I graduated from college having majored in political science and history (double major) at McGill University in Montreal, Canada in '04. I've been working in various project management roles pretty much ever since (approx. 12 years experience) while learning code in my spare time."
   },
  {
    title: 'My Skillset',
-   description: "I possess a sharp eye for detail, which I use to find even the smallest errors in my code.  I work well under pressure and can produce high-quality work in short periods of time.  I have strong interpersonal skills and have worked with a wide variety of people in a many different regions (Canada, Scotland, Washington DC, etc.)"
+   description: `Postgres, MySQL, Ruby on Rails, React/Redux.  Also comfortable with NodeJS & Angular.  Familiar with but still learning C++.  After that maybe Java.  The learning shall not stop.
+   I possess a sharp eye for detail, which I use to find even the weirdest bugs in my code. I work well under pressure and can produce high-quality work in short periods of time. I have strong interpersonal skills and have worked with a wide variety of people in a many different regions (Canada, Scotland, Washington DC, etc.)
+   Certified Project Manager (PMI, PRINCE2) and Change Manager (Prosci).  Resume available upon request. ` 
+   
   },
   {
     title: 'My Goals',
-    description: "I am looking for any and all opportunities to put my coding skills to good use. I'm fascinated with the concept of creating programs from nothing but code. Hoping to make it more than just a hobby someday soon."
+    description: "Code is my passion.  I am looking for any and all opportunities to put my coding skills to good use. I'm fascinated with the concept of creating programs from nothing but text. Hoping to make it more than just a hobby someday soon."
   },
 
 
 ]
+
+
+
 export default medata
