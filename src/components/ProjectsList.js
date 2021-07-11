4263817ebbba0753712e88d2d0230e196a1fd22b
christopherdent <christopherdent01@gmail.com>
2021-07-11 17:09:44 -0400

    tweaks to projects section

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -9,12 +9,12 @@ const ProjectsList = (props) => {
     <Container fluid >
         <h1 className='heading' id='projects'>My Projects</h1>
 <br />
-<p id='desc'>Check out my portfolio projects from my time at Flatiron School below. <strong>Please be patient while they load.</strong> App back-ends are all hosted on Heroku, which puts apps to sleep when they aren't being used so you might to wait a minute or even hit 'refresh' if nothing's happening. Trust me, it's worth the wait. For apps that require a login, feel free to make one! Or, use demo accounts with username guest, password guest.</p>
+<p id='desc'>Check out my portfolio projects from my time at Flatiron School below. <strong>Please be patient while they load.</strong> App back-ends are all hosted on Heroku, which puts apps to sleep when they aren't being used.  For apps that require a login, feel free to make one! Or, use demo account with username guest, password guest.</p>
            <CardDeck>
              {props.listProjects}
             </CardDeck>
             <br/>
-            <h4 className='page-title'>more coming soon...</h4>
+            <h4 className='page-title'>+ this React-based website itself.  <br/>more coming soon...</h4>
           </Container>
      </React.Fragment>
    )
