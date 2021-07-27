eb4c054ee80834585dc1a46375a1512d1396ef20
christopherdent <christopherdent01@gmail.com>
2021-07-26 22:26:43 -0400

    Recentered and resized navbar text

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 
 const MenuBar = (props) => {
   return (
-<div>
+<>
+
   <Navbar bg="dark" variant="dark" fixed="top" id='nav'>
-    <Navbar.Brand href="#home">CjD</Navbar.Brand>
-    <Nav className="mr-auto">
     
-      <Nav.Link href="#projects">   Projects   </Nav.Link>
-      <Nav.Link href="#"></Nav.Link>
-      <Nav.Link href="#about">   About Me</Nav.Link>
-      <Nav.Link href="#"></Nav.Link>
-      <Nav.Link href="#blog">My Blog</Nav.Link>
-    </Nav>
-  </Navbar>
-</div>
+    <Navbar.Brand href="#home">          CjD          </Navbar.Brand>
+   
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      
+        <Navbar.Collapse id="basic-navbar-nav">
+        
+          <Nav className="ml-auto" id='navbar'>
+    
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            
+            <Nav.Link href="#about">About Me</Nav.Link>
+            <Nav.Link href="#blog">My Blog</Nav.Link>
+  
+          </Nav>
+           
+        </Navbar.Collapse>
+       
+    </Navbar>
+  
+  < />
   );
 }
 export default MenuBar
