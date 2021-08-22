921a0837773d5b63d4ea980ade55b4e8e6fd1a96
christopherdent <christopherdent01@gmail.com>
2021-08-22 19:33:27 -0400

    remove redundant css files and remove text decoration on blog

--- src/components/MyBlog.js
+++ src/components/MyBlog.js
@@ -13,7 +13,7 @@ return (
 <a href = "https://christopher-dent.medium.com/">
 <Image id='blog' src="https://img1.wsimg.com/isteam/ip/61a4c061-55f8-47d5-8bd5-cb2c89e202c9/family-blog.jpg" fluid />
 <br />
-<span className = 'blinking'><h1 className="heading">Read My Blog?</h1></span>
+<span className = 'blinking'><h1 className="heading" id='blog-blink'>Read My Blog?</h1></span>
 </a>
 <br />
 
