f1234f9190ce9ba66056d150e040044add241347
Christopher Dent <christopherdent01@gmail.com>
2021-10-04 21:54:32 -0400

    created links to nowhere on upcoming apps

--- src/small_project_data.js
+++ src/small_project_data.js
@@ -3,8 +3,8 @@ const smallprojects = [
     {
       name: 'Angular Newsletter Signup',
       imageUrl: 'https://www.dictionary.com/e/wp-content/uploads/2018/04/under-construction-300x300.jpg',
-      appUrl: '/',
-      demoUrl: '',
+      appUrl: '#',
+      demoUrl: '#',
       code: 'https://github.com/christopherdent/newsletterExample', 
       code2: 'https://github.com/christopherdent/news-backend',
       description: 'My first foray into Angular, a simple newsletter signup application.  Lanch expected mid-October 2021.'
@@ -21,9 +21,9 @@ const smallprojects = [
     {
       name: 'Pomagotchi',
       imageUrl: 'https://www.pngfind.com/pngs/m/655-6552249_pomeranian-clipart-head-cartoon-pomeranian-png-transparent-png.png',
-      appUrl: '/',
+      appUrl: '#',
       code: 'https://github.com/christopherdent/pomagotchi', 
-      code2: '/',
+      code2: '#',
       description: 'A virtual pet pomeranian.  This is just a placeholder; the app is still in early stages of development.'
     },
 
