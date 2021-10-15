40cb4bf2ec2c36f92055f75d7ee047436db3c976
Christopher Dent <christopherdent01@gmail.com>
2021-10-14 20:55:49 -0400

    minor css update

--- src/components/SmallProjectsList.js
+++ src/components/SmallProjectsList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { CardDeck, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
+import Modal from 'react-bootstrap/Modal';
 
 const SmallProjectsList = (props) => {
 
