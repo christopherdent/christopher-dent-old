2ba8467549174bc009bc8d37234f1aed5749f7a3
Christopher Dent <christopherdent01@gmail.com>
2021-10-21 22:30:12 -0400

    update social

--- src/components/Social.js
+++ src/components/Social.js
@@ -13,7 +13,7 @@ const Social = (props) => {
 
       <br />
       <SocialIcon url="https://www.linkedin.com/in/christopher-james-dent/" id='blog' style={{ height: 100, width: 100 }} />
-      <SocialIcon url="https://www.facebook.com/christopher.j.dent" id='blog' style={{ height: 100, width: 100 }} />
+      
       <SocialIcon url="https://www.youtube.com/channel/UCZBxvf3wO37Ymx-mO8tPwLQ" id='blog' style={{ height: 100, width: 100 }} />
       <SocialIcon url="https://github.com/christopherdent" id='blog' style={{ height: 100, width: 100 }} />
       <SocialIcon url="mailto: christopherdent01@gmail.com" id="blog" style={{ height: 100, width: 100 }} />
